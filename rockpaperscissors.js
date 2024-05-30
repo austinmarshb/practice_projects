d7005329c28fe7e5882820abb3d9438954957d7c
Austin Marsh <168210295+austinmarshb@users.noreply.github.com>
2024-05-30 15:40:40 -0400

    Add files via upload

---
+++ rockpaperscissors.js
@@ -0,0 +1,28 @@
+const getUserChoice = (userInput) => {
+  userInput = userInput.toLowerCase();
+  if (
+    userInput === "rock" ||
+    userInput === "scissors" ||
+    userInput === "paper"
+  ) {
+    return userInput;
+  } else {
+    console.log("Error, please type: rock, paper or scissors.");
+  }
+};
+
+const getComputerChoice = () => {
+  const randomNumber = Math.floor(Math.random() * 3);
+  switch (randomNumber) {
+    case 0:
+      return "rock";
+    case 1:
+      return "paper";
+    case 2:
+      return "scissors";
+  }
+};
+
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
